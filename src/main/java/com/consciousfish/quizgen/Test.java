b0ba1fc183d55c7c07e92a8878db44dd429628de
Sachit Mishra <smishra2@ufl.edu>
2014-09-07 18:18:43 -0400

    Using gradle now. Updated everything accordingly

--- src/main/java/com/consciousfish/quizgen/Test.java
+++ src/main/java/com/consciousfish/quizgen/Test.java
@@ -1,10 +1,12 @@
+package com.consciousfish.quizgen;
+
 import java.util.Properties;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
+		System.out.println("yo it worked.");
 	}
 
 }
