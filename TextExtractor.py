8a220639f95025b311aa900909a3107791175c06
Sachit Mishra <smishra2@ufl.edu>
2014-11-30 10:19:20 -0500

    Used alchemy api and output text to file

--- TextExtractor.py
+++ TextExtractor.py
@@ -1,23 +1,37 @@
 __author__ = 'Sachit'
 
-import http.client
 import urllib.parse
+import urllib.request
+import json
 
 class TextExtractor:
+
+    ALCHEMYURL = 'http://access.alchemyapi.com/calls/url'
+    ALCHEMYENDPOINT = "/URLGetText"
+    APIKEY = "d83d5968892fd5865f3cc74e5e6bfac124db9267"
+    OUTPUTMODE = "json"  
 
     def __init__(self, website):
         self.website = website
 
     def extract(self):
         print("TextExtractor extracting " + self.website)
-        #conn = http.client.HTTPConnection("http://boilerpipe-web.appspot.com")
-        #params = urllib.parse.urlencode({'url': self.website,
-        #                                 'output': 'text',
-        #                                 'extractor': 'ArticleExtractor'})
-        #conn.request("GET", "/extract", params)
-        conn = http.client.HTTPConnection("127.0.0.1:5000")
-        params = urllib.parse.urlencode({'testID': 3})
-        conn.request("GET", "/test", params)
-        response = conn.getresponse()
-        print(response.status)
-        conn.close()
+
+        # Get text from Alchemy
+        data = {}
+        data['url'] = self.website
+        data['apikey'] = self.APIKEY
+        data['outputMode'] = self.OUTPUTMODE
+        url_values = urllib.parse.urlencode(data)
+        full_url = self.ALCHEMYURL + self.ALCHEMYENDPOINT + '?' + url_values
+        response = urllib.request.urlopen(full_url)
+        try:
+            json_response = response.read().decode('utf-8').strip()
+            decoded_json = json.loads(json_response)
+        except (ValueError, KeyError, TypeError) as err:
+            print("JSON format error " + err.reason)
+
+        # Put text in file
+        f = open('res/Input', 'w')
+        f.write(decoded_json['text'])
+        f.close
