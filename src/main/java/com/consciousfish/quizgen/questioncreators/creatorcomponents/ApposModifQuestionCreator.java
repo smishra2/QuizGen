cb546845d0deddb0b6e57e2f760ee76205c1195a
Sachit Mishra <smishra2@ufl.edu>
2014-12-04 10:56:45 -0500

    Added appos modifier question

--- src/main/java/com/consciousfish/quizgen/questioncreators/creatorcomponents/ApposModifQuestionCreator.java
+++ src/main/java/com/consciousfish/quizgen/questioncreators/creatorcomponents/ApposModifQuestionCreator.java
@@ -1,76 +1,109 @@
 package com.consciousfish.quizgen.questioncreators.creatorcomponents;
 
+import com.consciousfish.quizgen.FileIO;
 import com.consciousfish.quizgen.interfaces.Question;
 import com.consciousfish.quizgen.interfaces.QuestionCreator;
 import edu.stanford.nlp.dcoref.CorefChain;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.ArraySet;
 import edu.stanford.nlp.util.CoreMap;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Sachit on 12/2/2014.
  */
-public class ApposModifQuestionCreator  implements QuestionCreator {
+public class ApposModifQuestionCreator implements QuestionCreator {
     private static final boolean test = true;
 
     public List<Question> createQuestion(List<CoreMap> sentences, Map<Integer, CorefChain> coreferences) {
         if(test) System.out.println("createQuestion called");
         List<Question> questions = new ArrayList<Question>();
         for(CoreMap sentence : sentences) {
-            if(test) System.out.println("Parsing sentence: " + sentence.toString());
-            Tree parseTree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+            try {
+                if (test)
+                    System.out.println("Parsing sentence: " + sentence.toString());
+                Tree parseTree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+                SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
 
-            for (SemanticGraphEdge startEdge : dependencies.findAllRelns(EnglishGrammaticalRelations.APPOSITIONAL_MODIFIER)) {
-                if (test) System.out.println("startEdge found: " + startEdge.toString());
+                for (SemanticGraphEdge startEdge : dependencies.findAllRelns(EnglishGrammaticalRelations.APPOSITIONAL_MODIFIER)) {
+                    if (test)
+                        System.out.println("startEdge found: " + startEdge.toString());
 
-                String question = "";
+                    String question = "";
 
-                // First check if source is NE, if so, use Who
-                if (startEdge.getSource().ner().equalsIgnoreCase("PERSON"))
-                   question = "Who is ";
-                else
-                    question = "What is ";
+                    // First check if source is NE, if so, use Who
+                    if (startEdge.getSource().ner().equalsIgnoreCase("PERSON"))
+                        question = "Who is";
+                    else
+                        question = "What is";
 
-                int openIndex = startEdge.getTarget().beginPosition();
-                while (sentence.toString().charAt(openIndex) != ',' &&
-                        sentence.toString().charAt(openIndex) != '(')
-                    openIndex--;
-
-                int closeIndex = startEdge.getTarget().endPosition();
-                while (sentence.toString().charAt(closeIndex) != ',' &&
-                        sentence.toString().charAt(closeIndex) != ')')
-                    closeIndex++;
-
-                question += sentence.toString().substring(openIndex + 1, closeIndex).trim();
-                question += "?";
-
-                final String questionclone = question;
-
-                questions.add(new Question() {
-                    final String q = questionclone;
-                    final String a = "yes";
-
-                    public String getQuestion() {
-                        return q;
+                    // Assemble treemap of all words you'll need
+                    Set<IndexedWord> words = assembleWords(startEdge.getTarget(),
+                            dependencies);
+                    TreeMap<Integer, String> mappedWords = new TreeMap<Integer, String>();
+                    for (IndexedWord word : words) {
+                        // Use pronoun replacer if necessary
+                        if (word.tag().equalsIgnoreCase("prp") ||
+                                word.tag().equalsIgnoreCase("prp$")) {
+                            List<IndexedWord> newWords =
+                                    PronounReplacer.replacePronoun(
+                                    word, sentences, coreferences);
+                            String wholeNewWord = "";
+                            for (IndexedWord newWord : newWords)
+                                wholeNewWord += " " + newWord.originalText();
+                            wholeNewWord = wholeNewWord.trim();
+                            mappedWords.put(word.index(), wholeNewWord);
+                        }
+                        else { // not a pronoun
+                            mappedWords.put(word.index(), word.originalText());
+                        }
                     }
 
-                    public String getAnswer() {
-                        return a;
-                    }
-                });
+                    for (Integer index : mappedWords.keySet())
+                        question += " " + mappedWords.get(index);
 
+                    question += "?";
+
+                    final String questionclone = question;
+
+                    questions.add(new Question() {
+                        final String q = questionclone;
+                        final String a = "yes";
+
+                        public String getQuestion() {
+                            return q;
+                        }
+
+                        public String getAnswer() {
+                            return a;
+                        }
+                    });
+
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                FileIO.writeToLog(e);
             }
         }
         return questions;
+    }
+
+    private Set<IndexedWord> assembleWords(IndexedWord root,
+                                           SemanticGraph dependencies) {
+        Set<IndexedWord> set = new HashSet<IndexedWord>();
+        set.add(root);
+        for (SemanticGraphEdge edge : dependencies.outgoingEdgeIterable(root)) {
+            set.addAll(assembleWords(edge.getTarget(), dependencies));
+        }
+        return set;
     }
 }
 
