9996df2d2cc60d9df136555eb74083a8bf3a0ef8
Sachit Mishra <smishra2@ufl.edu>
2014-12-06 17:15:46 -0500

    Question is now abstract. Implements equals()

--- src/main/java/com/consciousfish/quizgen/interfaces/Question.java
+++ src/main/java/com/consciousfish/quizgen/interfaces/Question.java
@@ -3,7 +3,15 @@ package com.consciousfish.quizgen.interfaces;
 /**
  * Created by Jonathan on 2014/10/30.
  */
-public interface Question {
-    public String getQuestion();
-    public String getAnswer();
+//public interface Question {
+public abstract class Question {
+    public abstract String getQuestion();
+    public abstract String getAnswer();
+
+    public boolean equals(Object object) {
+        if (object instanceof Question)
+            return getQuestion().equals(((Question) (object)).getQuestion());
+        else
+            return false;
+    }
 }
