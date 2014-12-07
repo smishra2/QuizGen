5e2211c7bd486c29e4b8a113dd5ee0cdff9f8a6b
unknown <jonathan.howe.yu@gmail.com>
2014-12-06 21:09:08 -0500

    Changed JSON format

--- src/main/java/com/consciousfish/quizgen/JsonOutput.java
+++ src/main/java/com/consciousfish/quizgen/JsonOutput.java
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -19,14 +20,16 @@ public class JsonOutput {
         this.output = output;
     }
 
-    public void sendOutput(List<Question> questions) {
-        JsonArray json = new JsonArray();
+    public void sendOutput(Collection<Question> questions) {
+        JsonObject json = new JsonObject();
+        JsonArray array = new JsonArray();
         for(Question q : questions) {
             JsonObject obj = new JsonObject();
             obj.addProperty("question", q.getQuestion());
             obj.addProperty("sentence", q.getSentence());
-            json.add(obj);
+            array.add(obj);
         }
+        json.add("questions", array);
 
         /*JsonObject json = new JsonObject();
         JsonArray questionsArray = new JsonArray();
